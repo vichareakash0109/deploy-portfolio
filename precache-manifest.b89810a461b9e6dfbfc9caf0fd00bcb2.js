3a50ded35227b6ea7b0c4386d3f4504727344563
vichareakash0109 <vichareakash92@gmail.com>
2019-09-20 20:21:06 -0400

    Updates

---
+++ precache-manifest.b89810a461b9e6dfbfc9caf0fd00bcb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b793a7138931a42703e57357d4c31724",
+    "url": "/deploy-portfolio/index.html"
+  },
+  {
+    "revision": "05535a8cf9d90a424919",
+    "url": "/deploy-portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "56e856d20b34cc400e69",
+    "url": "/deploy-portfolio/static/css/main.3977a7b0.chunk.css"
+  },
+  {
+    "revision": "05535a8cf9d90a424919",
+    "url": "/deploy-portfolio/static/js/2.43a00905.chunk.js"
+  },
+  {
+    "revision": "56e856d20b34cc400e69",
+    "url": "/deploy-portfolio/static/js/main.692028b0.chunk.js"
+  },
+  {
+    "revision": "f4bb6ae1b09902cba03f",
+    "url": "/deploy-portfolio/static/js/runtime-main.2fe36e66.js"
+  }
+]);
